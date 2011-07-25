b786bde441788a575f112d2fee54ae75fade5602
cehars <cehars@bil.omu.edu.tr>
2011-07-25 16:25:45 +0300

    first

---
+++ index.php
@@ -0,0 +1,51 @@
+/*
+Açılır Menü
+*/
+<script language="JavaScript">
+<!-- 
+// 
+function go(){
+if (document.selecter.select1.options[document.selecter.select1.selectedIndex].value != "none") {
+location = document.selecter.select1.options[document.selecter.select1.selectedIndex].value
+ }                       
+}
+//-->
+</script>
+<BASEFONT SIZE="2" face="Arial">
+<FORM name="selecter">
+<SELECT name="select1" size=5>
+<OPTION value="http://www.turkcell.com.tr/turk/4x/sms/index.htm">Turkcell Mesaj Servisi
+<OPTION value="https://212.58.18.140/servlet/com.turkcell.cis.SessionManagerServlet?service=BillingInfo&lang=tr">Turkcell Fatura Bilgileri
+<OPTION value="http://www.telsim.com.tr/cgi-bin/sms/sms.cgi">Telsim - Mail Servisi
+<OPTION value="http://212.65.128.10/fatura.nsf/fatura">Telsim Fatura Bilgileri
+<OPTION value="http://www.datascope.com.tr/ise.htm">Borsa Şirketleri Analizleri
+<OPTION value="http://www.osym.gov.tr/sonuclar/index.html">ÖSYM Sınav Sonuçları
+<OPTION value="http://www.meb.gov.tr/sinavlar.htm">M.E.B Sınav Sonuçları
+<OPTION value="http://www.hotelguide.com.tr">Türkiye Otel Rehberi
+<OPTION value="http://www.englishpractice.com">İngilizce Online Pratik
+<OPTION value="https://orderonline.demiryatirim.com.tr/orderonline/">Demirbank Order Online
+<OPTION value="http://www.turkish-fltbooking.com/tkh1t.htm">THY Rezervasyon İşlemleri
+<OPTION value="http://www.turnet.net.tr/143tarife.html">Turnet Kontör Tarifesi
+<OPTION value="http://www.tcmb.gov.tr/kurlar/today.html">Merkez Bankası Döviz Kurları
+<OPTION value="http://plasma.nationalgeographic.com/mapmachine/">Elektronik Dünya Atlası
+<OPTION value="http://www.ada.net.tr/sanalkan/">Sanal Kan Arama Merkezi
+<OPTION value="http://www.emekli.gov.tr/">Emekli Sandığı Maaş/İkramiye Hes.
+<OPTION value="http://www.millipiyango.gov.tr/html/ustyeni.shtml">Milli Piy. ve Say. Loto Sonuçları
+<OPTION value="http://madhaus.cns.utoronto.ca/tpc/tpc_home.html">Ücretsiz Faks Servisi
+<OPTION value="http://www.dhmiata.gov.tr">Atatürk Havalimanı Geliş-Gidiş
+<OPTION value="http://weather.yahoo.com/regional/Turkey.html">Hava Durumu
+<OPTION value="http://www.beyazperde.com">Sinema Sayfaları
+<OPTION value="http://www.devtiyatro.gov.tr">Devlet Tiyatroları
+<OPTION value="http://www.lezzetmekan.com">Lezzet Mekanları
+<OPTION value="http://www.travlang.com">Birçok Dilde Çeviri
+<OPTION value="http://www.myphonebook.com">Sanal Adres Defteri
+<OPTION value="http://www.123greetings.com">Elektronik Kart Gön.Serv.
+<OPTION value="ftp://ftp.itu.edu.tr/pub">FTP 
+<OPTION value="http://www.turkishodyssey.com">Turkish Odyssey
+</SELECT>
+	<!TD ALIGN="CENTER" VALIGN="right">
+	<BASEFONT SIZE="2">
+	<INPUT TYPE="button" VALUE="Git" onclick="go()">
+	</FORM>
+--------------------------------------------------------------------------------------------------------------------
+
